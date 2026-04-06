42b09ba1ad5afc16498408d4471611b9b01936f1
Chance-Chappell <166332437+Chance-Chappell@users.noreply.github.com>
2026-04-05 23:02:24 -0400

    Add files via upload

---
+++ Module3_ChanceChappell.py
@@ -0,0 +1,8 @@
+# SDEV 120 - Module Assignment
+# This program welcomes the user and asks for their name
+
+print("Welcome to SDEV 120 class")
+
+name = input("Enter your name: ")
+
+print("Hello " + name)
